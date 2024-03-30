14868a463de9df0589c74ad1ca44b5938d449f02
Anand R P <anandrp.sics@gmail.com>
2024-03-30 17:32:20 +0530

    backend files added

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./DBConnection");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const userRoutes = require("./users/userRoutes.js");
+const orphanageRoutes = require("./orphanages/orphanageRoutes.js");
+const organizationRoutes = require("./organizations/organizationRoutes.js");
+const donationRequestRoutes = require("./donationRequests/donationRequestRoutes.js");
+const donationRoutes = require("./donation/donationRoutes.js");
+const { campRoutes } = require("./camp/campRoutes.js");
+
+app.use(express.static(`${__dirname}/upload`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Tvm Community working" });
+});
+
+app.use("/user", userRoutes);
+app.use("/orphanage", orphanageRoutes);
+app.use("/organization", organizationRoutes);
+app.use("/donation", donationRoutes);
+app.use("/donation-request", donationRequestRoutes);
+app.use("/camp", campRoutes)
+app.all("/*", (req, res) => {
+  res.status(404).json({ message: "Route not found." });
+});
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log("Server started successfully on port ", PORT);
+});
